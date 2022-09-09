0524b1d7fb94db9fa47c09a656a5ab61154820bf
Chibu <atuchibueze@gmail.com>
2022-09-08 21:57:37 -0400

    create pagination component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 import Posts from "./components/Posts";
+import Pagination from "./components/Pagination";
 
 const App = () => {
   const [posts, setPosts] = useState([]);
@@ -21,11 +22,14 @@ const App = () => {
   }, []);
 
   // Get current posts
-
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
   return (
     <div className="container mt-5">
       <h1 className="text-primary mb-3">My Blog</h1>
-      <Posts posts={posts} loading={loading} />
+      <Posts posts={currentPosts} loading={loading} />
+      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} />
     </div>
   );
 };
