dbf796ac25e92a8a9a9e8b7607caf7c88c4a8c52
Chibu <atuchibueze@gmail.com>
2022-09-08 21:03:16 -0400

    implement fetchPosts function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react'
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPosts(res.data);
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
   return (
     <div className="App">
-     <h1>My App</h1>
+      <h1>My App</h1>
     </div>
   );
-}
+};
 
 export default App;
